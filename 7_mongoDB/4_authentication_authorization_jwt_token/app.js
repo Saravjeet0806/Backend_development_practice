b0329690870aabb79398cc97f30854b951b08248
Sarav <Saravjeetofficial@gmail.com>
2025-06-09 20:23:37 +0530

    login and logout functionality

--- 7_mongoDB/4_authentication_authorization_jwt_token/app.js
+++ 7_mongoDB/4_authentication_authorization_jwt_token/app.js
@@ -4,6 +4,7 @@ const app = express()
 const path = require('path')
 const userModel = require('./models/user')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 app.set('view engine', 'ejs')
 app.use(express.json())
@@ -19,6 +20,7 @@ app.post('/create', async (req, res) => {
     let { username, email, password, age } = req.body
 
     bcrypt.genSalt( 10, (err, salt) => { //generate salt
+
         bcrypt.hash(password, salt, async (err, hash) => {
             let createdUser = await userModel.create({
                 username,
@@ -26,10 +28,39 @@ app.post('/create', async (req, res) => {
                 password: hash ,  // Store the hashed password
                 age
             })
+
+            let token = jwt.sign({ email}, "secretkey")
+            res.cookie('token', token) // Set the token in a cookie
+
              res.send(createdUser)
         })
     })
 
 })
+
+app.get('/login', async (req, res) => {
+    res.render('login')
+})
+
+app.post('/login', async (req, res) => {
+    let user = await userModel.findOne({ email: req.body.email })
+
+    if(!user) res.send("Something went wrong")
+    
+    bcrypt.compare(req.body.password, user.password, function(err, result) { // Compare the password with the hashed password
+        if(result) {
+            let token = jwt.sign({email: user.email}, "secretkey") // Create a JWT token
+            res.cookie('token', token) // Set the token in a cookie
+            res.send("Login successful")
+        }
+        else res.send("Something went wrong")
+    })   
+})
+
+app.get('/logout', (req, res) => {
+    res.cookie('token', "")
+    res.redirect('/')
+})
+
 
 app.listen(3000)
